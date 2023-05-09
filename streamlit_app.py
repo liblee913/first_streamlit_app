f098773e812ef64103daf5d5d845e38b2ee90117
liblee913 <132442337+liblee913@users.noreply.github.com>
2023-05-09 14:56:32 +0100

    Update streamlit_app.py
    Edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,6 @@ sl.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 sl.text(fruityvice_response)
-
+sl.header("Fruityvice Fruit Advice!")
 
 
