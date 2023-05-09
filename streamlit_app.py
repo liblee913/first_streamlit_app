35b6653f36f9047a680640ec77f4aa2141476765
liblee913 <132442337+liblee913@users.noreply.github.com>
2023-05-09 10:53:38 +0100

    Update streamlit_app.py
    Added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
-import streamlit 
+import streamlit as sl
 
-streamlit.title('My Parents New Healthy Diner')
+sl.title('My Parents New Healthy Diner')
+sl.subheader('Breakfast Menu')
+sl.text_input('Omega 3 & Bluberry Oatmeal')
+sl.text_input('Kale, Spinach & Rocket Smoothie')
+sl.text_input('Hard-Boiled Free-Range Egg')
