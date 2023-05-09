c1eb54ba0f23eda926068d305c739fc582b39094
liblee913 <132442337+liblee913@users.noreply.github.com>
2023-05-09 10:58:18 +0100

    Update streamlit_app.py
    Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as sl
+import pandas as pd
 
 sl.title('My Parents New Healthy Diner')
 sl.subheader('Breakfast Menu')
@@ -8,3 +9,8 @@ sl.text('🐔 Hard-Boiled Free-Range Egg')
 sl.text('🥑🍞 Avocado Toast')
 
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+sl.dataframe(my_fruit_list)
+
+
