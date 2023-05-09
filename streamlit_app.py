d30bb3750b79332153047bbe91a8be201a944257
liblee913 <132442337+liblee913@users.noreply.github.com>
2023-05-09 15:51:43 +0100

    Update streamlit_app.py
    added

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ sl.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-sl.text("Hello from Snowflake:")
-sl.text(my_data_row)
+my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+sl.text("The fruit load list contains:")
+sl.text(my_data_rows)
 
